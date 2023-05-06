38cab85b111f316febfeee49b15155b84b4ee1c0
Davi Silva <davisilvaphoto@gmail.com>
2023-05-06 17:31:00 -0300

    primitive types - pt5 - exercise

---
+++ src/academy/devdojo/maratonajava/intro/Lesson03PrimitiveTypesExercise.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.intro;
+
+public class Lesson03PrimitiveTypesExercise {
+    public static void main(String[] args) {
+        String name = "Davi Silva";
+        String address = "Serra de Peroba, nº90";
+        double salary = 1500.00;
+        String date = "06/05/2023";
+        String finalMessage = "Eu " + name + " morando no endereço " + address + " confirmo que recebi o salário de R$" + salary + ", na data " + date;
+
+        System.out.println(finalMessage);
+    }
+}
