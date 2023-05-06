3471ead3b92eab9f8e7acd3bc853b8a76e5c6493
Davi Silva <davisilvaphoto@gmail.com>
2023-05-06 16:37:13 -0300

    comments

--- src/academy/devdojo/maratonajava/intro/HelloWoldDevDojo.java
+++ src/academy/devdojo/maratonajava/intro/HelloWoldDevDojo.java
@@ -1,6 +1,10 @@
 package academy.devdojo.maratonajava.intro;
 
+/**
+ * This is <b>javadoc</b> comment
+ */
 public class HelloWoldDevDojo {
+    // this is a line comment
     public static void main(String[] args) {
         System.out.println("Hello World DEVDOJO");
     }
