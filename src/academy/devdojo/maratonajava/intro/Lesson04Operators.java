4cd254f0aaa626cd9f549363cde91cd399ee3961
Davi Silva <davisilvaphoto@gmail.com>
2023-05-08 08:05:15 -0300

    operators - pt01

---
+++ src/academy/devdojo/maratonajava/intro/Lesson04Operators.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.intro;
+
+public class Lesson04Operators {
+    public static void main(String[] args) {
+        // Math operators: + - / *
+        int numberOne = 10;
+        int numberTwo = 20;
+
+        int result = numberTwo - numberOne;
+        System.out.println(result);
+        result = numberTwo + numberOne;
+        System.out.println(result);
+        result = numberTwo / numberOne;
+        System.out.println(result);
+        result = numberTwo * numberOne;
+        System.out.println(result);
+
+    }
+}
