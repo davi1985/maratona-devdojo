a43e91b17fe5c9e52830facc16fd1139963709de
Davi Silva <davisilvaphoto@gmail.com>
2023-05-08 08:16:46 -0300

    operators - pt02

--- src/academy/devdojo/maratonajava/intro/Lesson04Operators.java
+++ src/academy/devdojo/maratonajava/intro/Lesson04Operators.java
@@ -2,18 +2,24 @@ package academy.devdojo.maratonajava.intro;
 
 public class Lesson04Operators {
     public static void main(String[] args) {
-        // Math operators: + - / *
+        // Math operators: %
         int numberOne = 10;
         int numberTwo = 20;
 
-        int result = numberTwo - numberOne;
-        System.out.println(result);
-        result = numberTwo + numberOne;
-        System.out.println(result);
-        result = numberTwo / numberOne;
-        System.out.println(result);
-        result = numberTwo * numberOne;
-        System.out.println(result);
+        int rest = 20 % 7;
+        System.out.println(rest);
 
+        // comparator operators: < > <= >= == != - always return boolean value
+        boolean isNumberHigherThanOther = numberOne > numberTwo;
+        boolean isNumberMinorThanOther = numberOne < numberTwo;
+        boolean isNumberEqualsToOther = numberOne == numberTwo;
+        boolean isFirstNumberIsEqual = numberOne == numberOne;
+        boolean isNumberIsDifferentThanOther = numberOne != numberTwo;
+
+        System.out.println("isNumberHigherThanOther: " + isNumberHigherThanOther);
+        System.out.println("isNumberMinorThanOther: " + isNumberMinorThanOther);
+        System.out.println("isNumberEqualsToOther: " + isNumberEqualsToOther);
+        System.out.println("isFirstNumberIsEqual: " + isFirstNumberIsEqual);
+        System.out.println("isNumberIsDifferentThanOther: " + isNumberIsDifferentThanOther);
     }
 }
