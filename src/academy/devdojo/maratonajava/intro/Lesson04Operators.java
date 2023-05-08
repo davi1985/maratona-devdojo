a6c9965162c36029b82b238dfa1a846f91d75fe5
Davi Silva <davisilvaphoto@gmail.com>
2023-05-08 08:45:25 -0300

    operators - pt03 - logic OR

--- src/academy/devdojo/maratonajava/intro/Lesson04Operators.java
+++ src/academy/devdojo/maratonajava/intro/Lesson04Operators.java
@@ -2,29 +2,13 @@ package academy.devdojo.maratonajava.intro;
 
 public class Lesson04Operators {
     public static void main(String[] args) {
-        int numberOne = 10;
-        int numberTwo = 20;
-
-        // comparator operators: < > <= >= == != - always return boolean value
-        boolean isNumberHigherThanOther = numberOne > numberTwo;
-        boolean isNumberMinorThanOther = numberOne < numberTwo;
-        boolean isNumberEqualsToOther = numberOne == numberTwo;
-        boolean isFirstNumberIsEqual = numberOne == numberOne;
-        boolean isNumberIsDifferentThanOther = numberOne != numberTwo;
-
-        System.out.println("isNumberHigherThanOther: " + isNumberHigherThanOther);
-        System.out.println("isNumberMinorThanOther: " + isNumberMinorThanOther);
-        System.out.println("isNumberEqualsToOther: " + isNumberEqualsToOther);
-        System.out.println("isFirstNumberIsEqual: " + isFirstNumberIsEqual);
-        System.out.println("isNumberIsDifferentThanOther: " + isNumberIsDifferentThanOther);
-
         // logic operators: &&(and), || (or), !(not)
-        int age = 35;
-        float salary = 3500F;
-        boolean isInsideLowParameterHigherThanThirty = age > 30 && salary >= 4612.00;
-        boolean isInsideLowParameterMinorThanThirty = age < 30 && salary > 3381.00;
+        double currentAccountTotalAmount = 200;
+        double savingAccountTotalAmount = 10000;
+        float playStationAmount = 5000F;
 
-        System.out.println(isInsideLowParameterHigherThanThirty);
-        System.out.println(isInsideLowParameterMinorThanThirty);
+        boolean canBuyPlayStationFive = currentAccountTotalAmount > playStationAmount || savingAccountTotalAmount > playStationAmount;
+
+        System.out.println("Can buy PlayStation 5 ? " + canBuyPlayStationFive);
     }
 }
