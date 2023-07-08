6cacaa7a9dc38719c4915a82e70a1a4d48d0f992
Davi Silva <davisilvaphoto@gmail.com>
2023-07-08 17:02:58 -0300

    Lesson 05 switch case ex

---
+++ src/Lesson05Conditionals06.java
@@ -0,0 +1,22 @@
+public class Lesson05Conditionals06 {
+    public static void main(String[] args) {
+        byte day = 1;
+
+        switch (day) {
+            case 1:
+            case 7:
+                System.out.println("weekend");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Work day");
+                break;
+
+            default:
+                System.out.println("Invalid");
+        }
+    }
+}
